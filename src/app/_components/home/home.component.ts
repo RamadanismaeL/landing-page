673d5ea8a4235a7d00753e7af34f95674d498b98
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-11 23:59:54 +0200

    footer updated

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { GroupPhotoComponent } from "../group-photo/group-photo.component";
+import { InfoComponent } from "../info/info.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, BtnPrimaryComponent, NewsletterFormComponent],
+  imports: [HeaderComponent, BtnPrimaryComponent, NewsletterFormComponent, GroupPhotoComponent, InfoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
