1223cec84125ba498dfa0e23ce60752de57fa986
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-11 14:52:06 +0200

    form updated

--- src/app/_components/newsletter-form/newsletter-form.component.ts
+++ src/app/_components/newsletter-form/newsletter-form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
-// 2:07:39
+  newsletterForm!: FormGroup;
+
+  constructor()
+  {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
 }
